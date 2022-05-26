0b39d48e01aa5aa1b53663de9faf25911c23df07
JDavid80 <jdcalderong@gmail.com>
2022-05-26 08:55:28 -0500

    menu responsive

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menuBarra = document.getElementById("menuBarra");
+const mainMenu = document.getElementById("mainMenu");
+
+menuBarra.addEventListener("click", function() {
+    if (mainMenu.classList.contains("menu-vertical-oculto")) {
+        mainMenu.classList.add("menu-vertical");
+        mainMenu.classList.remove("menu-vertical-oculto");
+    }
+    else {
+        mainMenu.classList.remove("menu-vertical");
+        mainMenu.classList.add("menu-vertical-oculto");
+    }
+    /*  */
+    /* if (mainMenu.classList.contains("menu-vertical")) {
+        alert("Holis..");
+    } */ 
+});
